8076a5816472a1a967449d97c44001d3d88990f2
KaosMC <38406827+KaosMC@users.noreply.github.com>
2018-04-29 15:37:33 +0300

    Add files via upload

---
+++ help.js
@@ -0,0 +1,21 @@
+const Discord = require("discord.js");
+
+module.exports.run = async (bot, message, args) => {
+    let channel = message.channel;
+
+    let embedMsg = new Discord.RichEmbed()
+        .setColor("#42f4bf")
+        .addField("?help", "View this page.", true)
+        .addField("?getrole", "Get a character's role.", true)
+        .addField("?hello", "Get those mean replies.", true)
+        .addField("?defaultrole", "Requires special permission -- Modifies the default role.", true)
+        .addField("?setprefix", "Requires special permission -- Modifies bot prefix.", true);
+
+    channel.send(embedMsg).then(embedMsg => {
+        embedMsg.delete(10000).catch(err => console.log(`An error has occured: \n` + err));
+    });
+}
+
+module.exports.help = {
+    name: "help"
+}
