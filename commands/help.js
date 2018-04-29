c82bca39fd32879e5a115b69e3b4cd44114ad6a3
KaosMC <38406827+KaosMC@users.noreply.github.com>
2018-04-29 15:49:45 +0300

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -1,14 +1,16 @@
 const Discord = require("discord.js");
+const botconfig = require("../botconfig.json");
 
 module.exports.run = async (bot, message, args) => {
     let channel = message.channel;
+    let prefix = botconfig.prefix;
 
     let embedMsg = new Discord.RichEmbed()
         .setColor("#42f4bf")
-        .addField("?help", "View this page.", true)
-        .addField("?getrole", "Get a character's role.", true)
-        .addField("?hello", "Huh, get those mean replies.", true)
-        .addField("?setprefix", "Modifies bot prefix.", true);
+        .addField(`${prefix}help`, "View this page.", true)
+        .addField(`${prefix}getrole`, "Get a character's role.", true)
+        .addField(`${prefix}hello`, "Huh, get those mean replies.", true)
+        .addField(`${prefix}setprefix`, "Modifies bot prefix.", true);
 
     channel.send(embedMsg).then(embedMsg => {
         embedMsg.delete(10000).catch(err => console.log(`An error has occured: \n` + err));
