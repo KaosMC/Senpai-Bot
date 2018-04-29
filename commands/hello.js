a2752d97adb437108cc62bcbb43b1fb39c99af0f
KaosMC <38406827+KaosMC@users.noreply.github.com>
2018-04-29 15:43:47 +0300

    Update hello.js

--- commands/hello.js
+++ commands/hello.js
@@ -2,8 +2,10 @@ const Discord = require("discord.js");
 
 module.exports.run = async (bot, message, args) => {
     var replies = new Array();
-    replies.push("Huh, I'm busy now.");
-    replies.push("Not available now, maybe later?");
+    replies.push("I'm busy.");
+    replies.push("Don't you have anything better to do?");
+    replies.push("Hey yourself.");
+    replies.push("Maybe talk to the hundreds of other humans in this server.");
 
     var r = Math.floor(Math.random() * replies.length);
 
