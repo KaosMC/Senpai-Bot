e96253d7c3f9f0c596d5456096be42f7ae2963ef
KaosMC <38406827+KaosMC@users.noreply.github.com>
2018-04-30 22:38:23 +0300

    Add files via upload

--- commands/help.js
+++ commands/help.js
@@ -1,18 +1,15 @@
 const Discord = require("discord.js");
-const botconfig = require("../botconfig.json");
 
 module.exports.run = async (bot, message, args) => {
-    message.delete();
-    
     let channel = message.channel;
-    let prefix = botconfig.prefix;
 
     let embedMsg = new Discord.RichEmbed()
         .setColor("#42f4bf")
-        .addField(`${prefix}help`, "View this page.", true)
-        .addField(`${prefix}getrole`, "Get a character's role.", true)
-        .addField(`${prefix}hello`, "Huh, get those mean replies.", true)
-        .addField(`${prefix}setprefix`, "Modifies bot prefix.", true);
+        .addField("?help", "View this page.", true)
+        .addField("?getrole", "Get a character's role.", true)
+        .addField("?hello", "Get those mean replies.", true)
+        .addField("?ily", "Get those mean replies, part 2.", true)
+        .addField("?setprefix", "Modifies bot prefix.", true);
 
     channel.send(embedMsg).then(embedMsg => {
         embedMsg.delete(10000).catch(err => console.log(`An error has occured: \n` + err));
